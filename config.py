be1f5c5390283200cb97c4bb78ab430a7a68efd7
Danish <danishali9575@gmail.com>
2025-05-16 19:06:09 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-AZURE_AI_ENDPOINT = "https://mydocreaderai.cognitiveservices.azure.com/"
-AZURE_AI_API_KEY = "A2wLV6lI8qaqJb6tkJmzeX7xolmQEMfiI6WxLZf6iCaeyYnlqDkpJQQJ99BCACGhslBXJ3w3AAALACOGP3B6"
-AZURE_AI_MODEL_NAME = "NeuralV4"  # Replace with your model name
-
-# Azure Blob Storage configuration
-BLOB_URL = "https://certificatereader.blob.core.windows.net/"
-SAS_TOKEN = "?sp=rw&st=2025-03-12T12:22:38Z&se=2026-03-02T20:22:38Z&spr=https&sv=2022-11-02&sr=c&sig=rquXnEmi3vvg2SCfgUFr74pH3EfASD7b7cJMHg5iN6c%3D"
-CONTAINER_NAME = "certificatecontainer"
